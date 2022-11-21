aa5adc42c5935f50429fed198c1b842f590527ec
kabdomora <114969831+kabdomora@users.noreply.github.com>
2022-11-20 18:48:39 -0800

    api link to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var userContainer = document.getElementById('users');
 var fetchButton = document.getElementById('fetch-button');
 
 function getApi() {
-  var requestUrl = 'https://api.github.com/users?per_page=5';
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=34.063229&lon=-118.359651&appid=c27a1f4c454fb85c78f18c582a3a25ef';
 
   fetch(requestUrl)
     .then(function (response) {
@@ -13,16 +13,19 @@ function getApi() {
     .then(function (data) {
       // Use the console to examine the response
       console.log(data);
-      userContainer.innerHTML = "";
-      // TODO: Loop through the data and generate your HTML
-      for (var i = 0; i < data.length; i++) {
-        var userName = document.createElement("h3");
-        var userURL = document.createElement("p");
-        userName.textContent = data[i].login;
-        userURL.textContent = data[i].url;
-        userContainer.append(userName);
-        userContainer.append(userURL);
-      }
+
     });
 }
-fetchButton.addEventListener('click', getApi);
+// fetchButton.addEventListener('click', getApi);
+
+
+// userContainer.innerHTML = "";
+// // TODO: Loop through the data and generate your HTML
+// for (var i = 0; i < data.length; i++) {
+//   var userName = document.createElement("h3");
+//   var userURL = document.createElement("p");
+//   userName.textContent = data[i].login;
+//   userURL.textContent = data[i].url;
+//   userContainer.append(userName);
+//   userContainer.append(userURL);
+// }
