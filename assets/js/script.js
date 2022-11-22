9eac902ec8634db20cd16d7b99eaa90503df4e3f
kabdomora <114969831+kabdomora@users.noreply.github.com>
2022-11-22 03:55:27 -0800

    list searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // copied from 06-Act-10. Use for reference for api call
 
-var resultContainer = document.getElementById('recents');
+var searchHistory = document.getElementById('recents');
 var fetchButton = document.getElementById('fetch-button');
 var searchInput = document.getElementById('city-search');
 var msgDiv = document.querySelector("#msg");
@@ -15,24 +15,18 @@ function displayMessage(type, message) {
 }
 
 function listSearches() {
-  var searched = localStorage.getItem('searchCity');
-  resultContainer.innerHTML = "";
+  searchHistory.innerHTML ="";
 
   for (var i=0; i < searches.length; i++) {
-    var sItem = searches[i];
+    var searched = searches[i];
 
-    var sButton = document.createElement('button');
-    sButton.textContent = searched;
+    var city = document.createElement('button');
+    city.textContent = searched;
+    city.setAttribute('class', "w-full lg:flex items-center text-sm leading-6 text-slate-400 rounded-md ring-1 ring-slate-900/10 shadow-sm py-1.5 pl-2 pr-3 hover:ring-slate-300 dark:bg-slate-800 dark:highlight-white/5 dark:hover:bg-slate-700");
 
+    searchHistory.appendChild(city);
   }
 }
-
-/* <button type="button" 
-class="w-full lg:flex items-center text-sm leading-6 text-slate-400 
-rounded-md ring-1 ring-slate-900/10 shadow-sm py-1.5 pl-2 pr-3 
-hover:ring-slate-300 dark:bg-slate-800 dark:highlight-white/5 
-dark:hover:bg-slate-700"> <span id="recent">Recently Searched</span>
-</button> */
 
 function getApi() {
   var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=34.063229&lon=-118.359651&appid=c27a1f4c454fb85c78f18c582a3a25ef';
